90a27f88d615ef884ac0106a3a846b57fa72c5c5
Artem Dmitriev <dmitriev_ar@nlmk.com>
2024-11-05 21:41:58 +0300

    Fix error

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Logo, BurgerIcon, ListIcon, ProfileIcon,} from '@ya.praktikum/react-developer-burger-ui-components';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import headerStyles from './app-header.module.css';
 
 const AppHeader = () => {
