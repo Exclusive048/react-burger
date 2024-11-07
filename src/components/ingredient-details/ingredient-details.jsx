5480641872b25ef72394b89df47e0eae501f7849
Artem Dmitriev <dmitriev_ar@nlmk.com>
2024-11-08 01:27:28 +0300

    Добавлена надпись "Детали ингредиента"

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -3,30 +3,35 @@ import PropTypes from "prop-types";
 
 const IngredientDetails = (props) =>{
     return (
-        <div className={ingredientDetailsStyle.mainContainer}>
-            <img alt={props.ingredientImg.img} src={props.ingredientImg}/>
-            <p className={`${ingredientDetailsStyle.ingredientNameText}text text_type_main-medium mt-4`}>
-                {props.ingredientName}
+        <>
+            <p className={`${ingredientDetailsStyle.ingredientHeaderText} text text_type_main-medium mt-4`}>
+            Детали ингредиента
             </p>
-            <div className={`${ingredientDetailsStyle.macronutrientContainer} mt-8 mb-15`}>
-                <div className={ingredientDetailsStyle.macronutrientElem}>
-                    <p className="text text_type_main-small text_color_inactive">Калории,ккал</p>
-                    <p className="text text_type_digits-default text_color_inactive">{props.ingredientCalories}</p>
-                </div>
-                <div className={ingredientDetailsStyle.macronutrientElem}>
-                    <p className="text text_type_main-small text_color_inactive">Белки, г</p>
-                    <p className="text text_type_digits-default text_color_inactive">{props.ingredientProtein}</p>
-                </div>
-                <div className={ingredientDetailsStyle.macronutrientElem}>
-                    <p className="text text_type_main-small text_color_inactive">Жиры, г</p>
-                    <p className="text text_type_digits-default text_color_inactive">{props.ingredientFat}</p>
-                </div>
-                <div className={ingredientDetailsStyle.macronutrientElem}>
-                    <p className="text text_type_main-small text_color_inactive">Углеводы, г</p>
-                    <p className="text text_type_digits-default text_color_inactive">{props.ingredeintCarbohydrates}</p>
+            <div className={ingredientDetailsStyle.mainContainer}>
+                <img alt={props.ingredientImg.img} src={props.ingredientImg}/>
+                <p className={`${ingredientDetailsStyle.ingredientNameText}text text_type_main-medium mt-4`}>
+                    {props.ingredientName}
+                </p>
+                <div className={`${ingredientDetailsStyle.macronutrientContainer} mt-8 mb-15`}>
+                    <div className={ingredientDetailsStyle.macronutrientElem}>
+                        <p className="text text_type_main-small text_color_inactive">Калории,ккал</p>
+                        <p className="text text_type_digits-default text_color_inactive">{props.ingredientCalories}</p>
+                    </div>
+                    <div className={ingredientDetailsStyle.macronutrientElem}>
+                        <p className="text text_type_main-small text_color_inactive">Белки, г</p>
+                        <p className="text text_type_digits-default text_color_inactive">{props.ingredientProtein}</p>
+                    </div>
+                    <div className={ingredientDetailsStyle.macronutrientElem}>
+                        <p className="text text_type_main-small text_color_inactive">Жиры, г</p>
+                        <p className="text text_type_digits-default text_color_inactive">{props.ingredientFat}</p>
+                    </div>
+                    <div className={ingredientDetailsStyle.macronutrientElem}>
+                        <p className="text text_type_main-small text_color_inactive">Углеводы, г</p>
+                        <p className="text text_type_digits-default text_color_inactive">{props.ingredeintCarbohydrates}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
